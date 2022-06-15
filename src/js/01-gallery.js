ce95cdd81699290cba77caecb8a3630c1e778450
Bohdan Onyshko <96291948+Bohdan171@users.noreply.github.com>
2022-06-15 20:44:03 +0300

    1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const markupGallery = createMarkupGallery(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', markupGallery);
+
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" style="display: block" />
+        </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
